1b907034198c8d1e1175e203a36516a3a476e909
frank120121 <francisco.lopez4256@gmail.com>
2024-03-11 18:49:23 -0700

    Fix missing balance calculation and update logging configuration

--- binance_orders.py
+++ binance_orders.py
@@ -2,6 +2,8 @@ import asyncio
 from binance_wallets import BinanceWallets
 import traceback
 import logging
+from logging_config import setup_logging
+setup_logging(log_filename='Binance_c2c_logger.log')
 logger = logging.getLogger(__name__)
 
 async def get_wallets():
@@ -9,15 +11,15 @@ async def get_wallets():
     await wallets.balances()
     return wallets
 
-async def new_order(wallets, account_to_use, most_usd_asset, missing_balance):
+async def new_order(wallets, account_to_use, asset_type, most_usd_asset, missing_balance):
     try: 
         await wallets.place_order(
             api_key=wallets.credentials_dict[account_to_use]['KEY'],
             api_secret=wallets.credentials_dict[account_to_use]['SECRET'],
-            symbol=f"BTC{most_usd_asset}",
+            symbol=f"{asset_type}{most_usd_asset}",
             side="BUY",
             order_type="MARKET",
-            quantity=round(missing_balance, 5),
+            quantity=round(missing_balance, 4),
             price=None,
             timeInForce=None,
             quoteOrderQty=None
@@ -28,9 +30,8 @@ async def new_order(wallets, account_to_use, most_usd_asset, missing_balance):
 
 async def binance_buy_order(asset_type):
     try: 
-        btc_target = 0.25000
         wallets = await get_wallets()
-        missing_balance = wallets.check_asset_balance(asset_type, btc_target)
+        missing_balance = wallets.check_asset_balance(asset_type)
 
         logger.debug(f"Inside binance_buy_order for {asset_type}")
         logger.info(f"Missing balance for {asset_type}: {missing_balance}")
@@ -39,7 +40,7 @@ async def binance_buy_order(asset_type):
             account_to_use, most_usd_asset = wallets.get_account_with_most_usd()
             logger.debug(f'account to use:{account_to_use}, most usd asset: {most_usd_asset}')
 
-            await new_order(wallets, account_to_use, most_usd_asset, missing_balance)
+            await new_order(wallets, account_to_use, asset_type, most_usd_asset, missing_balance)
         
         else: 
             logger.info(f"No missing balance for {asset_type}; {missing_balance}")
@@ -50,7 +51,7 @@ async def binance_buy_order(asset_type):
 
 async def binance_orders_main(loop):
     await asyncio.gather(
-        binance_buy_order('ETH'),
+        binance_buy_order('BTC'),
     )
 
 if __name__ == "__main__":
